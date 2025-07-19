5b53acaa7b0b12299c92438039fd4802e9894668
yack <ychillitupa.231ds10@gmail.com>
2025-07-19 17:01:18 -0500

    fix(backend): Corrige la ruta de Socket.IO para alinearla con Nginx

--- index.js
+++ index.js
@@ -95,6 +95,7 @@ app.get('/api', (req, res) => res.json({ message: '¡Bienvenido a la API de la P
 // --- Socket.IO Configuration ---
 const io = new Server(server, {
   cors: corsOptions,
+  path: '/api/socket.io/', // Alinea la ruta con la configuración de Nginx
 });
 
 io.on('connection', (socket) => {
